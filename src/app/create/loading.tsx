6e509a6942c8e271a3a408cc625999d6db616839
mihiragath <mihiragath8@gmail.com>
2025-02-02 12:28:12 +0530

    new

---
+++ src/app/create/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function loading() {
+  return (
+    <div className="w-full h-dvh flex justify-center items-center"></div>
+  )
+}
+
+export default loading
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/Header";
+import Provider from "./Provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -42,7 +44,10 @@ export default function RootLayout({
             attribute="class"
             defaultTheme="dark"
           >
+            <Provider>
+            <Header />
             {children}
+            </Provider>
           </ThemeProvider>
       </body>
     </html>
